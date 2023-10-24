5548169b46dd041ad61d93be79b96cf2834227aa
Tristan <flfflrd@gmail.com>
2023-10-24 23:33:16 +1030

    updated to 2023 tristan@david

--- README.md
+++ README.md
@@ -2,24 +2,13 @@
 ## my linux dotfiles
 
 As the name implies, these are my personal dotfiles.\
-Just started maintaining these again so some big changes have been made.
 
-tristan@muhammad
-----------------
-os Ubuntu 20.04.1 LTS on Windows 10 [WSL2]
-host x86_64
-kernel 4.4.0-19041-Microsoft
-
-
-tristan@ibrahim
+tristan@david
 ---------------
-os Arch Linux
-host B450 GAMING X
-kernel 5.12.9-arch1-1
+Ubuntu WSL \
+5.15.90.1-microsoft-standard-WSL2 
 
 ### todo
 
-- [x] Make an easy install script
-- [x] Unify color theme with .Xres or gtk maybe
-- [x] Get a job
-- [ ] Keep a job
+- [] Make an easy install script
+
--- install.sh
+++ install.sh
@@ -4,17 +4,25 @@
 # | | | | \__ \ || (_| | | |_\__ \ | | |
 # |_|_| |_|___/\__\__,_|_|_(_)___/_| |_|
 #
-# Tristan Fulford's easy environment installer
+# Tristan Fulford's ezpz environment installer
 
-cd ~
-
-mkdir ~/bin 2>/dev/null
-[ ! -f ~/bin/pfetch ] && exec curl https://raw.githubusercontent.com/dylanaraps/pfetch/master/pfetch > ~/bin/pfetch
-chmod +x ~/bin/pfetch
+sudo apt install zsh
+echo zsh installed
 
 [ ! -d ~/.oh-my-zsh ] && sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo oh-my-zsh installed
 
-exec ln -sf ~/dots/.zshrc
-exec ln -sf ~/dots/.vimrc
+[ ! -d ~/.vim/bundle/Vundle.vim ] && git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+echo vundle installed
 
-exec git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+[ ! -d ~/.vim/colors ] && mkdir ~/.vim/colors
+curl https://raw.githubusercontent.com/morhetz/gruvbox/master/colors/gruvbox.vim > gruvbox.vim
+echo gruvbox for vim installed
+
+cp ~/.zshrc ~/.zshrc.pre-install
+cp ~/.vimrc ~/.vimrc.pre-install
+ln -sf ~/dots/.zshrc ~
+ln -sf ~/dots/.vimrc ~
+echo symlinks for zsh and vim rc\'s created
+
+# Missing conditionals and modularity.
